1a1185623630d3d8793039d432f61bb3ac834b64
arunjadhavt <ajadhav@talend.com>
2020-06-26 11:42:44 -0700

    new nexus disk

--- spec/acceptance/shared/nexus.rb
+++ spec/acceptance/shared/nexus.rb
@@ -28,6 +28,19 @@ shared_examples 'profile::nexus' do
       )
     end
   end
+  
+  describe file('/data') do
+    it do
+      should be_mounted.with(
+        :type    => 'xfs',
+        :options => {
+          :rw         => true,
+          :noatime    => true,
+          :nodiratime => true
+        }
+      )
+    end
+  end
 
   describe 'nexus user deployment should be removed' do
     describe file('/srv/sonatype-work/nexus/conf/security.xml') do
